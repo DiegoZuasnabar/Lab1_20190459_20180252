783493e3600d94964def6284bb6dd5aae096e4d7
Luis <a20190459@pucp.edu.pe>
2023-04-24 11:40:55 -0500

    dfg

--- app/src/main/java/com/example/lab1_iot/DetailActivity.java
+++ app/src/main/java/com/example/lab1_iot/DetailActivity.java
@@ -1,14 +1,20 @@
 package com.example.lab1_iot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Window;
 
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        window.setStatusBarColor(Color.parseColor("#5fec1d"));
         setContentView(R.layout.activity_detail);
+
     }
 }
