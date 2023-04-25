e1a4ad35759b2c1aa28a784b9da80d02ba0b2513
Luis <a20190459@pucp.edu.pe>
2023-04-24 21:07:48 -0500

    la

--- app/src/main/java/com/example/lab1_iot/RegisterActivity.java
+++ app/src/main/java/com/example/lab1_iot/RegisterActivity.java
@@ -4,18 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button button2= findViewById(R.id.button2);
-        button2.setOnClickListener((view)->{
-            Intent intent = new Intent(this, RegisterActivity.class);
+        setContentView(R.layout.activity_register);
+        Button button5= findViewById(R.id.button5);
+        button5.setOnClickListener((view)->{
+            Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
     }
